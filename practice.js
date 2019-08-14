39d62702cc87466a7344fcde0d36f9352cbf2855
Takaya Sato <49809778+TakayaTanuki@users.noreply.github.com>
2019-08-14 13:09:47 +0900

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,11 @@
 let button = document.forms[0].btn;
 button.addEventListener('click', calcTax);
 
+//https://qiita.com/amamamaou/items/25e8b4e1b41c8d3211f4
+//https://qiita.com/rindarinda5/items/c26dc81fe8cd98992dc1
+//getElementByClassName⇒htmlCollectionのオブジェクトで値が返ってくる
+//getElementById⇒Idの値が返ってくる
+
 function calcTax() {
     let inputText = document.forms[0].beforeTax.value;
     const taxRate = 1.08;
@@ -9,3 +14,5 @@ function calcTax() {
     let afterText = document.forms[0].afterTax;
     afterText.value = afterTax;
 }
+//form名.form部品名
+const button = calcForm.btn;
