23bd117f412d163041557dda453e567d8250eee0
Takaya Sato <49809778+TakayaTanuki@users.noreply.github.com>
2019-08-14 17:30:12 +0900

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -78,4 +78,150 @@ if (isIOs) {
 }
 
 //
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+    <meta charset="UTF-8">
+    <title>じゃんけん！！</title>
+    <link rel="stylesheet" href="janken.css">
+    <script src="janken.js"></script>
+</head>
+
+<body>
+    <!--1段落目(メッセージ表示)-->
+    <!--Divタグにもclassやidを付与できるため、後ほど行うかも？？-->
+    <div>
+        <form action="javascript:void(0)" name="inputName">
+            <h3>プレイヤー名を入力して下さい。</h3>
+            <input type="text" name="inputPlayerName">
+            <input type="submit" value="OK" name="submit" onclick="changeMessage('none');">
+        </form>
+    </div>
+
+    <!--2段落目(じゃんけんの画像選択)-->
+    <div>
+        <!--できたら2,3段落目は最初非表示、名前が入力されたら表示できるようにする-->
+        <div>
+            <!--グーコマンド-->
+            <span class="guhParagraph">
+                <a href="#" onclick="selectGuh();">
+                    <img src="C:\Users\1815598\Desktop\Material\guu.jpg" id="guh">
+                </a>
+            </span>
+
+            <!--チョキコマンド-->
+            <span class="chokiParagraph">
+                <a href="#" onclick="selectChoki();">
+                    <img src="C:\Users\1815598\Desktop\Material\choki.jpg" id="choki">
+                </a>
+            </span>
+
+            <!--パーコマンド-->
+            <span class="pahParagraph">
+                <a href="#" onclick="selectPah();">
+                    <img src="C:\Users\1815598\Desktop\Material\pah.jpg" id="pah">
+                </a>
+            </span>
+        </div>
+
+        <!--3段落目(Result表示)-->
+        <div>
+            戦績
+            <table border="1">
+                <!--Player名列-->
+                <tr>
+                    <td></td>
+                    <td class="playerName"></td>
+                    <!--<span class="playerName"></span>でも同じ-->
+                    <td>aaa</td>
+                </tr>
+                <!--1戦目-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <!--2戦目-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <!--3戦目-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <!--4戦目-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <!--5戦目-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <!--勝敗-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
+let disp;
+
+function changeMessage(disp) {
+    const playerName = inputName.inputPlayerName.value;
+
+    //playerName == ''
+    //空文字の判定
+    if (!playerName) {
+        disp = 'block';
+    }
+
+    //Player名をセットする
+    document.querySelector('.playerName').innerText = playerName;
+
+    //Formを非表示に
+    inputName.style.display = disp;
+
+}
+
+//let guh = document.getElementById('guh');
+//guh.addEventListener('click', selectGuh);
+function selectGuh() {
+    alert('グー！');
+
+}
+
+function selectChoki() {
+    alert('チョキ！');
+
+}
+
+function selectPah() {
+    alert('パー！');
+
+}
+
+.guhParagraph{
+    width : 100px ;
+}
+
+.chokiParagraph{
+    width : calc(100% / 3) ;
+}
+
+.pahParagraph{
+    width : calc(100% / 3) ;
+}
